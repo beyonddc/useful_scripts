0de69dfcdeaba2ace4da7e0c61b5d05882b3da14
Chun Tat David Chu <beyonddc@gmail.com>
2014-11-10 21:39:23 -0500

    Created the Perl directory and added a script

---
+++ Perl/print_no_dup.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+#-----------------------------------------------------------------------------#
+# This script read, remove all duplication, sort each line then print it
+#-----------------------------------------------------------------------------#
+
+use File::Basename;
+
+my $filename = $ARGV[0];
+
+if ($filename eq "--help") {
+
+    my $basename = basename($0);
+    print "Usage: $basename FILENAME\n";
+
+} else {
+
+    print "#-----------------------------------------------------------------------------#\n";
+    print "Opening file $filename\n";
+    print "#-----------------------------------------------------------------------------#\n";
+
+    open (FILE, "<$filename") or die "Couldn't open $filename, $!\n";
+
+    my %hash = ();
+    while (<FILE>) {
+        $hash{$_} = $_;
+    }
+    close (FILE);
+
+    for my $key (sort keys %hash) {
+        print "$hash{$key}";
+    }
+}
